51585389682ec5594fdeb38c5ef89498ce8c402f
Maya Gillilan <101652314+MayaGillilanContentful@users.noreply.github.com>
2023-06-08 11:17:05 +0200

    raise on good pairs

--- src/Player.ts
+++ src/Player.ts
@@ -5,7 +5,36 @@ export class Player {
     const playerIndex = gameState.in_action;
     const currentBet = players[playerIndex]["bet"];
     const callAmt = currentBuyin - currentBet;
-    betCallback(callAmt);
+    const minRaise = gameState.minimum_raise;
+    const holeCards = gameState.players[playerIndex]["hole_cards"];
+    const firstCard = holeCards[0];
+    const secondCard = holeCards[1];
+
+    switch (holeCards) {
+      case firstCard.rank === secondCard.rank:
+        switch (firstCard.rank) {
+          case "A":
+            betCallback(callAmt + minRaise * 3);
+            break;
+          case "K":
+            betCallback(callAmt + minRaise * 2);
+            break;
+          case "Q":
+            betCallback(callAmt + minRaise * 2);
+            break;
+          case "J":
+            betCallback(callAmt + minRaise);
+            break;
+          case "10":
+            betCallback(callAmt + minRaise);
+            break;
+          default:
+            betCallback(callAmt);
+        }
+        break;
+      default:
+        betCallback(callAmt);
+    }
   }
 
   public showdown(gameState: any): void {}
