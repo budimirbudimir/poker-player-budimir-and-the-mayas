86ee3787877d839ab02dd8808f72a4f17c4f700f
Maya Gillilan <101652314+MayaGillilanContentful@users.noreply.github.com>
2023-06-08 11:03:40 +0200

    always call

--- src/Player.ts
+++ src/Player.ts
@@ -1,11 +1,14 @@
 export class Player {
   public betRequest(gameState: any, betCallback: (bet: number) => void): void {
-    betCallback(0);
+    const currentBuyin = gameState.current_buy_in;
+    const players = gameState.players;
+    const playerIndex = gameState.in_action;
+    const currentBet = players[playerIndex]["bet"];
+    const callAmt = currentBuyin - currentBet;
+    betCallback(callAmt);
   }
 
-  public showdown(gameState: any): void {
-
-  }
-};
+  public showdown(gameState: any): void {}
+}
 
 export default Player;
