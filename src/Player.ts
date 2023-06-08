b4435a25815b39556664209d1aebb78f8b787370
Maya Gillilan <101652314+MayaGillilanContentful@users.noreply.github.com>
2023-06-08 12:15:19 +0200

    fix logic

--- src/Player.ts
+++ src/Player.ts
@@ -20,57 +20,53 @@ export class Player {
     const firstCard = holeCards[0];
     const secondCard = holeCards[1];
 
-    switch (holeCards) {
-      case firstCard.rank === secondCard.rank:
-        switch (firstCard.rank) {
-          case "A":
-            betCallback(callAmt + minRaise * 6);
-            break;
-          case "K":
-            betCallback(callAmt + minRaise * 5);
-            break;
-          case "Q":
-            betCallback(callAmt + minRaise * 4);
-            break;
-          case "J":
-            betCallback(callAmt + minRaise * 3);
-            break;
-          case "10":
-            betCallback(callAmt + minRaise * 2);
-            break;
-          default:
-            betCallback(callAmt + minRaise);
+    if (firstCard.rank === secondCard.rank) {
+      switch (firstCard.rank) {
+        case "A":
+          betCallback(callAmt + minRaise * 6);
+          break;
+        case "K":
+          betCallback(callAmt + minRaise * 5);
+          break;
+        case "Q":
+          betCallback(callAmt + minRaise * 4);
+          break;
+        case "J":
+          betCallback(callAmt + minRaise * 3);
+          break;
+        case "10":
+          betCallback(callAmt + minRaise * 2);
+          break;
+        default:
+          betCallback(callAmt + minRaise);
+      }
+    } else if (firstCard.suit === secondCard.suit) {
+      if (firstCard.rank === "A" || secondCard.rank === "A") {
+        if (firstCard.rank === "K" || secondCard.rank === "K") {
+          //A+K suited
+          betCallback(callAmt + minRaise * 4);
         }
-        break;
-      case firstCard.suit === secondCard.suit:
-        if (firstCard.rank === "A" || secondCard.rank === "A") {
-          if (firstCard.rank === "K" || secondCard.rank === "K") {
-            //A+K suited
-            betCallback(callAmt + minRaise * 4);
-          }
-          if (firstCard.rank === "Q" || secondCard.rank === "Q") {
-            // A+Q suited
-            betCallback(callAmt + minRaise * 3);
-          }
-          if (firstCard.rank === "J" || secondCard.rank === "J") {
-            // A+J suited
-            betCallback(callAmt + minRaise * 3);
-          }
-        } else if (firstCard.rank === "K" || secondCard.rank === "K") {
-          if (firstCard.rank === "Q" || secondCard.rank === "Q") {
-            // K+Q suited
-            betCallback(callAmt + minRaise * 3);
-          }
-        } else {
-          betCallback(callAmt);
+        if (firstCard.rank === "Q" || secondCard.rank === "Q") {
+          // A+Q suited
+          betCallback(callAmt + minRaise * 3);
         }
-        break;
-      case (firstCard.rank === "A" || firstCard.rank === "K") &&
-        (secondCard.rank === "A" || secondCard.rank === "K"):
-        betCallback(callAmt + minRaise * 2);
-        break;
-      default:
-        betCallback(callAmt);
+        if (firstCard.rank === "J" || secondCard.rank === "J") {
+          // A+J suited
+          betCallback(callAmt + minRaise * 3);
+        }
+      }
+    } else if (firstCard.rank === "K" || secondCard.rank === "K") {
+      if (firstCard.rank === "Q" || secondCard.rank === "Q") {
+        // K+Q suited
+        betCallback(callAmt + minRaise * 3);
+      }
+    } else if (
+      (firstCard.rank === "A" || firstCard.rank === "K") &&
+      (secondCard.rank === "A" || secondCard.rank === "K")
+    ) {
+      betCallback(callAmt + minRaise * 2);
+    } else {
+      betCallback(callAmt);
     }
   }
 
